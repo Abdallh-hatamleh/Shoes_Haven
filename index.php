a8407fd944d1e64f5606510bfa6fc029ac5226bf
Abdullah <Abdallh.a.hatamleh@gmail.com>
2024-08-01 11:49:03 +0300

    i want to cry

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <!-- Linking SwiperJS CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
   <link rel="stylesheet" href="css/slider.css">
+  <link rel="stylesheet" href="css/style.css">
   <!-- <link rel="stylesheet" href="css/testimonials.css"> -->
 </head>
 
