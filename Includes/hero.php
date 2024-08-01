e04e0341cf61fd1a7455d69a83c6118e44097461
Abdallh-hatamleh <abdallh.a.hatamleh@gmail.com>
2024-08-01 20:29:50 +0300

    index gathered

---
+++ Includes/hero.php
@@ -0,0 +1,10 @@
+<div class="hero">
+        <div class="hero-content">
+            <h1 class="main">CHOOSE <span class="style1">YOUR</span> STYLE</h1><br><br>
+            <h2 class="text">Your ultimate destination for trendy and comfortable shoes.</h2><br><br>
+            <a href="#shop" class="btn" id="shop-btn">Check collection</a>
+        </div>
+    </div>
+    <br>
+    <br>
+  
--- index.php
+++ index.php
@@ -10,12 +10,16 @@
   <link rel="stylesheet" href="css/slider.css">
   <link rel="stylesheet" href="css/testimonials.css">
   <link rel="stylesheet" href="css/nav.css">
-    <link rel="stylesheet" href="css/foter.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link href="https://fonts.googleapis.com/css2?family=Jockey+One&family=Jomhuria&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/foter.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 
 <body style="position:relative">
   <?php include_once("includes/nav.php") ?>
+  <?php include("includes/hero.php") ?>
+  <?php include("includes/about.php") ?>
   <div class="category">
         <h2>formal</h2>
         <?php include("Includes\slidertop.php")?>
