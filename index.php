eb546a7b0fff3d6692d77c5477093efa90778f3c
Alhillawii <ayahhillawi9@gmail.com>
2024-08-05 09:28:21 +0300

    testiomnials

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
   <!-- Linking SwiperJS CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
   <link rel="stylesheet" href="css/slider.css">
-  <link rel="stylesheet" href="css/testimonials.css">
+  <link rel="stylesheet" href="css/testimonialss.css">
   <link rel="stylesheet" href="css/nav.css">
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/styless.css">
   <link href="https://fonts.googleapis.com/css2?family=Jockey+One&family=Jomhuria&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/foter.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -35,44 +35,33 @@
     <?php include("Includes/testimonialtop.php") ?>
         <div class="testimonial-card-item swiper-slide"><img src="Testimonial/images/img-1.jpg" alt="User Image" class="testimonial-user-image">
           <h2 class="testimonial-user-name">James Wilson</h2>
-          <article class="testimonial-message-article">Message Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Magni
-            laborum est facilis mollitia tenetur, aspernatur voluptatibus, laudantium qui </article>
+          <article class="testimonial-message-article">Shoes Haven is my go-to store for all my footwear needs. The quality and variety are unmatched!
+          </article>
         </div>
 
         <div class="testimonial-card-item swiper-slide"><img src="Testimonial/images/img-2.jpg" alt="User Image" class="testimonial-user-image">
           <h2 class="testimonial-user-name">Sarah Johnson</h2>
-          <article class="testimonial-message-article">Message Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Magni
-            laborum est facilis mollitia tenetur, aspernatur voluptatibus, laudantium qui </article>
+          <article class="testimonial-message-article">Amazing customer service and super fast delivery. I love shopping here!</article>
         </div>
 
         <div class="testimonial-card-item swiper-slide"><img src="Testimonial/images/img-3.jpg" alt="User Image" class="testimonial-user-image">
           <h2 class="testimonial-user-name">Michael Brown</h2>
-          <article class="testimonial-message-article">Message Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Magni
-            laborum est facilis mollitia tenetur, aspernatur voluptatibus, laudantium qui </article>
+          <article class="testimonial-message-article">The best online shopping experience I've ever had. Highly recommended! </article>
         </div>
 
         <div class="testimonial-card-item swiper-slide"><img src="Testimonial/images/img-4.jpg" alt="User Image" class="testimonial-user-image">
           <h2 class="testimonial-user-name">Emily Davis</h2>
-          <article class="testimonial-message-article">Message Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Magni
-            laborum est facilis mollitia tenetur, aspernatur voluptatibus, laudantium qui </article>
+          <article class="testimonial-message-article">Shoes Haven never disappoints. Great products and even better prices!</article>
         </div>
 
         <div class="testimonial-card-item swiper-slide"><img src="Testimonial/images/img-5.jpg" alt="User Image" class="testimonial-user-image">
           <h2 class="testimonial-user-name">Christopher Garcia</h2>
-          <article class="testimonial-message-article">Message Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Magni
-            laborum est facilis mollitia tenetur, aspernatur voluptatibus, laudantium qui </article>
+          <article class="testimonial-message-article">I found the perfect pair of shoes for my wedding here. Thank you, Shoes Haven!</article>
         </div>
 
         <div class="testimonial-card-item swiper-slide"><img src="Testimonial/images/img-6.jpg" alt="User Image" class="testimonial-user-image">
           <h2 class="testimonial-user-name">Richard Wilson</h2>
-          <article class="testimonial-message-article">Message Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Magni
-            laborum est facilis mollitia tenetur, aspernatur voluptatibus, laudantium qui </article>
+          <article class="testimonial-message-article">I had an excellent shopping experience at Shoes Haven. The website is user-friendly, and my order arrived quickly and exactly as described.</article>
         </div>
     <?php include("Includes/testimonialbottom.php") ?>
     </div>
